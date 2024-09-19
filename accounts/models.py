bc9aca08c3e751d84fc305e44dfc310a23e9cdf7
Ethan <ethans9999@gmail.com>
2024-09-19 18:58:56 +0200

    feat (accounts)
    Added email and password fields to the User model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class User(models.Model):
     
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=99)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     account_creation_date = models.DateTimeField(auto_now_add=True)
